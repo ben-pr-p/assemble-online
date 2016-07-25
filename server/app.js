16d2c40c8b86a2df141cc347654564a3fb932713
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2016-07-25 16:33:12 -0400

    easyrtc server draft

--- server/app.js
+++ server/app.js
@@ -1,12 +1,54 @@
 var log = require('debug')('assemble:app')
 var path = require('path')
 var express = require('express')
+var io = require('socket.io')
+var easyrtc = require('easyrtc')
+var http = require('http')
 var app = express()
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
 var staticDir = path.resolve(__dirname + '/../build')
 app.use('/', express.static(staticDir))
+
+var server = http.createServer(app)
+var socketServer = io.listen(server, {'log level':1})
+
+easyrtc.setOption('logLevel', 'debug')
+
+/*
+ * Overriding default listener to get logs
+ */
+easyrtc.events.on('easyrtcAuth', function(socket, easyrtcid, msg, socketCb, cb) {
+  easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCb, function(err, connectionObj){
+    if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
+      return cb(err, connectionObj)
+    }
+
+    connectionObj.setField('credential', msg.msgData.credential, {'isShared': false})
+    log(`${easyrtcid} credential saved`, connectionObj.getFieldValueSync('credential'))
+    cb(err, connectionObj)
+  })
+})
+
+/*
+ * Print out the credneitals for every room join
+ */
+easyrtc.events.on('roomJoin', function(connectionObj, roomName, roomParameter, cb) {
+  log(`${connectionObj.getEasyrtcid()} credential retrieved`, connectionObj.getFieldValueSync('credential'))
+  easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, cb)
+})
+
+/*
+ * Start EasyRTC server
+ */
+var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
+  log('initated easyrtc')
+
+  rtcRef.events.on('roomCreate', function(appObj, creatorConnectionObj, roomName, roomOptions, cb) {
+    log(`roomCreate fired! creating: ${roomName}`)
+
+    appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, cb)
+  })
+})
 
 var PORT = process.env.PORT
 if (!PORT) {
@@ -15,83 +57,4 @@ if (!PORT) {
 }
 
 log('Listening on PORT %d', PORT)
-http.listen(process.env.PORT || 3000)
-
-var users = []
-var sockets = {}
-var dimensions = {}
-
-io.on('connection', function (socket) {
-
-  socket.on('connect', function () {
-    log('New connection, sending users')
-    socket.emit('users', users)
-  })
-
-  socket.on('newuser', function (user) {
-    var existing = users.filter(u => u.id == user.id)[0]
-    if (existing) {
-      log('Got existing user %s update', user.id)
-      users.splice(users.indexOf(existing), 1)
-
-      user.x = existing.x
-      user.y = existing.y
-
-      users.push(user)
-      return socket.emit('users', users)
-    }
-
-    log('Got new user %s', user.id)
-
-    users.push(user)
-    sockets[user.id] = socket
-
-    socket.emit('users', users)
-  })
-
-  socket.on('movement', function(user) {
-    var moved = users.filter(u => u.id == user.id)[0]
-    moved.x = user.x
-    moved.y = user.y
-
-    setDimensions(users)
-
-    socket.emit('movement-update', {users, dimensions})
-  })
-
-  socket.on('disconnect', function () {
-    var user = users.filter(u => sockets[u.id] == socket)[0]
-    if (!user) {
-      log('Unknown user disconnect')
-    } else {
-      log('User %s disconnected', user.id)
-      users.splice(users.indexOf(user), 1)
-    }
-  })
-})
-
-function sendUpdates () {
-  users.forEach(u => {
-    if (sockets[u.id]) {
-      sockets[u.id].emit('users', users)
-    }
-  })
-  log('Sent updates to %d users', users.length)
-}
-
-function setDimensions (users) {
-  var maxPosX = maxPosY = maxScreenX = maxScreenY = 0
-
-  users.forEach(u => {
-    if (u.x > maxPosX) maxPosX = u.x
-    if (u.y > maxPosY) maxPosY = u.y
-    if (u.screenSize.x > maxScreenX) maxScreenX = u.screenSize.x
-    if (u.screenSize.y > maxScreenY) maxScreenY = u.screenSize.y
-  })
-
-  dimensions = {
-    x: Math.max(maxScreenX, maxPosX + (maxScreenX / 2)),
-    y: Math.max(maxScreenY, maxPosY + (maxScreenY / 2))
-  }
-}
-
+server.listen(PORT)
