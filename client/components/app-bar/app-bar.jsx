c1645b296b9b1ec609880931adeac4528b4d30d4
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2016-08-03 15:00:54 -0400

    no app bar

--- client/components/app-bar/app-bar.jsx
+++ client/components/app-bar/app-bar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import AppBar from 'material-ui/AppBar'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
 import IconButton from 'material-ui/IconButton'
 import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
-import NavigationClose from 'material-ui/svg-icons/navigation/close'
 import DeleteIcon from 'material-ui/svg-icons/action/delete'
 import AccountBox from 'material-ui/svg-icons/action/account-box'
+import Paper from 'material-ui/Paper'
 
 export default class AppBarIconMenu extends React.Component {
   constructor () {
@@ -23,35 +23,29 @@ export default class AppBarIconMenu extends React.Component {
   render () {
     const { roomName } = this.props
 
-    const style = {
-      position: 'fixed'
-    }
-
     return (
-      <AppBar
-        className='app-bar'
-        style={style}
-        title={roomName}
-        iconElementLeft={
-          <IconButton onClick={this.props.clearLocal} >
-            <DeleteIcon />
-          </IconButton>
-        }
-        iconElementRight={
+      <div className='app-bar'>
+        <Paper className='floating-button' circle={true} zDepth={5} style={{left: 'auto', right: 20}} >
           <IconMenu
             iconButtonElement={
-              <IconButton>
+              <IconButton className='circle-button' >
                 <AccountBox />
               </IconButton>
             }
-            targetOrigin={{horizontal: 'right', vertical: 'top'}}
-            anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+            targetOrigin={{horizontal: 'right', vertical: 'bottom'}}
+            anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
           >
             <MenuItem primaryText='Edit Me' onClick={this.editUser.bind(this)} />
             <MenuItem primateText='See Users' onClick={this.seeUsers.bind(this)} />
           </IconMenu>
-        }
-      />
+        </Paper>
+
+        <Paper className='floating-button' circle={true} zDepth={5} style={{left: 20, right: 'auto'}} >
+          <IconButton onClick={this.props.clearLocal} className='circle-button'  >
+            <DeleteIcon />
+          </IconButton>
+        </Paper>
+      </div>
     )
   }
 }
--- client/components/user-blob/user-blob.jsx
+++ client/components/user-blob/user-blob.jsx
@@ -14,6 +14,8 @@ const d = r * 2
 const sr = 25
 const sd = sr * 2
 
+const colorScale = d3.scale.ordinal().range(['#01df00', '#daff02', '#fe6634', '#008e82', '#00cfe2', '#fb0528', '#9b6304', '#532696', '#b53284', '#ff7ba6'])
+
 export default class UserBlob extends React.Component {
   constructor () {
     super()
@@ -26,7 +28,8 @@ export default class UserBlob extends React.Component {
     if (isNaN(x)) x = 0
     if (isNaN(y)) y = 0
 
-    const fill = user.avatar ? `url(#avatar-${user.id})` : this.fill
+    this.fill = `url(#avatar-${user.id})`
+    this.color = colorScale(this.props.idx)
 
     const adj = {
       x: x + translate.x,
@@ -34,13 +37,13 @@ export default class UserBlob extends React.Component {
     }
 
     if (!isMe && (adj.x < 0 || adj.x > window.screen.width || adj.y < 0 || adj.y > window.screen.height)) {
-      return this.renderFar(user, x, y, fill)
+      return this.renderFar(user, x, y)
     } else {
-      return this.renderClose(user, x, y, fill)
+      return this.renderClose(user, x, y)
     }
   }
 
-  renderFar (user, x, y, fill) {
+  renderFar (user, x, y) {
     const halfW = window.screen.width / 2
     const halfH = window.screen.height / 2
     const walls = {
@@ -59,16 +62,40 @@ export default class UserBlob extends React.Component {
       bottom: lineIntersect.checkIntersection(line.start.x, line.start.y, line.end.x, line.end.y, walls.bottom.start.x, walls.bottom.start.y, walls.bottom.end.x, walls.bottom.end.y)
     }
 
-    let point
+    let point, intersectingWall
     for (let wall in intersects) {
       if (intersects[wall].type == 'intersecting') {
         point = result.point
+        intersectingWall = wall
       }
     }
 
+    let dx, dy
+    switch (intersectingWall) {
+      case 'left':
+        dx = sr
+        dy = 0
+        break
+
+      case 'right':
+        dx = (-1) * sr
+        dy = 0
+        break
+
+      case 'top':
+        dx = 0
+        dy = sr
+        break
+
+      case 'bottom':
+        dx = 0
+        dy = (-1) * sr
+        break
+    }
+
     const p = {
-      x: point.x,
-      y: point.y
+      x: point.x + dx,
+      y: point.y + dy
     }
 
     return (
@@ -83,11 +110,11 @@ export default class UserBlob extends React.Component {
                 <image x='0' y='0' width={sd} height={sd} xlinkHref={user.avatar}></image>
               </pattern>
             </defs>
-            <circle transform={`translate(${pos.x},${pos.y})`} r={sr} fill={fill} strokeWidth='6px' stroke='black' />
+            <circle transform={`translate(${pos.x},${pos.y})`} r={sr} fill={this.fill} strokeWidth='6px' stroke='black' />
             <path
               transform={`translate(${pos.x},${pos.y})`}
               strokeWidth='6px'
-              stroke='red'
+              stroke={this.color}
               d={d3.svg.arc().innerRadius(r).outerRadius(r+1).startAngle(0).endAngle(pos.z / 20 * Math.PI)()}
              />
           </g>
@@ -96,7 +123,7 @@ export default class UserBlob extends React.Component {
     )
   }
 
-  renderClose (user, x, y, fill) {
+  renderClose (user, x, y) {
     return (
       <Motion
         defaultStyle={{x: 0, y: 0, z: 0}}
@@ -109,11 +136,11 @@ export default class UserBlob extends React.Component {
                 <image x='0' y='0' width={d} height={d} xlinkHref={user.avatar}></image>
               </pattern>
             </defs>
-            <circle transform={`translate(${pos.x},${pos.y})`} r={r} fill={fill} strokeWidth='6px' stroke='black' />
+            <circle transform={`translate(${pos.x},${pos.y})`} r={r} fill={this.fill} strokeWidth='6px' stroke='black' />
             <path
               transform={`translate(${pos.x},${pos.y})`}
               strokeWidth='6px'
-              stroke='red'
+              stroke={this.color}
               d={d3.svg.arc().innerRadius(r).outerRadius(r+1).startAngle(0).endAngle(pos.z / 20 * Math.PI)()}
              />
           </g>
