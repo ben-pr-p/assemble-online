28fdf8a972d7a36810fcc2161ee5e6a2d1ff40e6
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2016-08-16 11:54:14 -0400

    changed some colors

--- client/portal/main.jsx
+++ client/portal/main.jsx
@@ -125,10 +125,10 @@ export default class Portal extends React.Component {
             <span className='big-header'>assemble.live</span>
             <br/>
             <br/>
-            <span>In the future, this will be a secure, real time, audio and text based online meeting platform based on the exploitation of <em>space</em>.</span>
+            <span className='simple-text'>In the future, this will be a secure, real time, audio and text based online meeting platform based on the exploitation of <em>space</em>.</span>
             <br/>
             <br/>
-            <span>For now, please mess around with your friends, report bugs, and suggest features.</span>
+            <span className='simple-text'>For now, please mess around with your friends, report bugs, and suggest features.</span>
 
             <div className='room-status'>
               <h3>Publicly Joinable Rooms</h3>
