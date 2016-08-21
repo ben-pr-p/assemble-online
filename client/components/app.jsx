becb816615cf9f650cfb914dacb4896f14fd7d02
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2016-08-21 13:37:39 -0400

    moved roadmap to ROADMAP.md

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -13,48 +13,6 @@ import Room from './room/room'
 import customTheme from '../lib/custom-theme.js'
 import Boss from '../lib/boss'
 
-/**
- * TO DO
-  * see who posted announcements
-  * doodle / scheduling system
-  * kick out / mute for facillitator
-  * mode where only facillitator can make announcements
-  * location reference for squares
-  * Arrow key movement
-  * Extended user profiles
-    * view other users
-  * Create tests for performance
-  * Serve locations directly to user blobs
-  * Persist locations across refreshes
-  * Sandstorm.io
-  * Agenda setting widget
-  * Barriers
-  * Documents
-  * MS Paint
-  * Full accessibility - voice, keys, and mouse exclusive navigation
-  * WebRTC to PSTN signaling
-  * Disappearing trails
- */
-
-/**
- * DESIGN SUGGESTIONS
-  * transparent user overlaps
-  * on feedback - try black box around icon and black headers
-  * use greed red spectrum for agree disagree
- */
-
-/**
- * FEATURE SUGGESTIONS
-  * searchable public rooms sorted by number of active users
-    * if same number then alphabetized
-  * chat
-  * mute yourself option
-  * take a picture with camera
-  * click on heads and go to the person
-  * optional video communication
-  * badges for agreements
- */
-
 const goodBrowsers = ['Chrome', 'Chromium', 'Firefox', 'Mozilla', 'Opera', 'Bowser', 'Canary']
 
 export default class App extends React.Component {
